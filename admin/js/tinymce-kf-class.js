d8c7cb4d5c1a450769f05955a696f6f649263f00
audrasjb <[audrasjb@gmail.com]>
2017-03-26 14:35:33 +0200

    development

---
+++ admin/js/tinymce-kf-class.js
@@ -0,0 +1,28 @@
+(function() {
+	tinymce.PluginManager.add( 'tinymce_kf_class', function( editor, url ) {
+		// Add Button to Visual Editor Toolbar
+		editor.addButton('tinymce_kf_class', {
+			title: kfTranslations['kf_add_button'],
+			id: 'mce-wp-kf',
+			stateSelector: 'kf',
+			onclick: function() {
+				editor.windowManager.open({
+					title: 'Insert key figure',
+					body: [{
+						type: 'textbox',
+						name: 'figure',
+						label: 'Figure'
+					},{
+						type: 'textbox',
+						name: 'text',
+						label: 'Text'
+					}],
+					onsubmit: function( e ) {
+						editor.insertContent( '<span class="keyfigure_bloc"><span class="keyfigure_bloc_figure">' + e.data.figure + '</span><span class="keyfigure_bloc_text">' + e.data.text + '</span></span>' );
+					}
+				});				
+			}
+		});		
+
+	});
+})();
--- admin/kf-admin.php
+++ admin/kf-admin.php
@@ -21,7 +21,8 @@
 add_action( 'admin_enqueue_scripts', 'enqueue_styles_key_figures_admin' );
 function enqueue_styles_key_figures_admin() {
 	wp_enqueue_style( 'wp-color-picker' );
-//	wp_enqueue_style( 'kf-admin-styles', plugin_dir_url( __FILE__ ) . 'css/kf-admin.css', array(), '', 'all' );
+	wp_enqueue_style( 'kf-admin-styles', plugin_dir_url( __FILE__ ) . 'css/kf-admin.css', array(), '', 'all' );
+	add_editor_style( plugin_dir_url( __FILE__ ) . 'css/kf-admin-editor.css' );
 }
 	
 // Enqueue scripts
@@ -195,3 +196,91 @@ function kf_options_page(  ) {
 	<?php
 
 }
+
+/**
+ *
+ * Plugin button in WordPress visual editor
+ *
+ */
+
+class TinyMCE_KF {
+	/**
+	* Plugin constructor.
+	*/
+	function __construct() {
+		if ( is_admin() ) {
+			add_action( 'init', array(  $this, 'setup_tinymce_kf' ) );
+		}
+	}
+	/**
+	* Check if the current user can edit Posts or Pages, and is using the Visual Editor
+	* If so, add some filters
+	*/
+	function setup_tinymce_kf() {
+		// Check if the logged in WordPress User can edit Posts or Pages
+		// If not, don't register
+		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+        	return;
+		}
+
+		// Check if the logged in WordPress User has the Visual Editor enabled
+		// If not, don't register
+		if ( get_user_option( 'rich_editing' ) !== 'true' ) {
+			return;
+		}
+		// Setup filters
+		add_action( 'plugins_loaded', 'load_languages_tinymce_kf' );
+		add_action( 'before_wp_tiny_mce', array( &$this, 'translate_tinymce_kf' ) );
+		add_filter( 'mce_external_plugins', array( &$this, 'add_tinymce_kf' ) );
+		add_filter( 'mce_buttons_2', array( &$this, 'add_tinymce_kf_toolbar_button' ) );		
+	}	
+
+	/**
+	* Adds the plugin to the TinyMCE / Visual Editor instance
+	*	
+	* @param array $plugin_array Array of registered TinyMCE Plugins
+	* @return array Modified array of registered TinyMCE Plugins
+	*/
+	function add_tinymce_kf( $plugin_array ) {
+		$plugin_array['tinymce_kf_class'] = plugin_dir_url( __FILE__ ) . 'js/tinymce-kf-class.js';
+		return $plugin_array;
+	}
+
+	/**
+	* Plugin's internationalization 
+	*	
+	* First load translation files
+	* Then add translation strings to a javascript variable
+	*/
+	function load_languages_tinymce_kf() {
+	    load_plugin_textdomain( 'key-figures', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); 
+	}
+	// Adding i18n tinymce strings
+	function translate_tinymce_kf() {
+		$translations = json_encode(
+			array( 
+				'kf_add_button' 		=> __('Key figure', 'key-figures'),
+				'kf_delete_button' 	=> __('Delete figure', 'key-figures'),
+				'kf_figure_label' 		=> __('Figure', 'key-figures'),
+				'kf_figure_help' 		=> __('Enter number', 'key-figures'),
+				'kf_text_label' 		=> __('Text', 'key-figures'),
+				'kf_text_help' 		=> __('Enter text', 'key-figures'),
+				'kf_alert' 			=> __('Please select some text first', 'key-figures')
+			)
+		);
+		echo '<script>var kfTranslations = ' . $translations . ';</script>';
+	}
+
+	/**
+	* Adds a button to the TinyMCE / Visual Editor which the user can click
+	* to insert the kf node tag.
+	*
+	* @param array $buttons Array of registered TinyMCE Buttons
+	* @return array Modified array of registered TinyMCE Buttons
+	*/
+	function add_tinymce_kf_toolbar_button( $buttons ) {
+		array_push( $buttons, 'tinymce_kf_class' );
+		return $buttons;
+	}
+}
+$TinyMCE_kf = new TinyMCE_KF;
